605e1c92b5504449ceb641cf8edbdb3f81165221
Wågen <kwa045@uib.no>
2022-03-29 23:53:26 +0200

    implemented methods for initialization

--- src/usable/event.js
+++ src/usable/event.js
@@ -5,6 +5,18 @@ export default function useEvents() {
     const getEvents = ref([])
 
     const setEvents = async () => {
+        getEvents.value = await fetchEvents
+    }
 
+    const createEvent = async (event) => {
+        storeEvent(event)
+    }
+
+    const updateEvent = async (event, id) => {
+        muteateEvent(event, id)
+    }
+
+    const deleteEvent = async (id) => {
+        destroyEvent(id)
     }
 }
