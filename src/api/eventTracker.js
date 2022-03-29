14d3b7748a688be99db85dc2cd32246b5319fba2
Wågen <kwa045@uib.no>
2022-03-29 23:46:51 +0200

    added more fetch methods

--- src/api/eventTracker.js
+++ src/api/eventTracker.js
@@ -1,6 +1,32 @@
 const baseURL = 'https://localhost:3000'
 
 export const fetchEvents = async () => {
-    const response = await fetch(`${baseURL}/events`)
+    const response = await fetch(`${baseURL}/event`)
     return await response.json()
+}
+
+export const storeEvent = async (event) => {
+    return await fetch(`${baseURL}/event`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(event)
+    })
+}
+
+export const mutateEvent = async (event, id) => {
+    return await fetch(`${baseURL}/event/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(event)
+    })
+}
+
+export const destroyEvent = async (id) => {
+    return await fetch(`${baseURL}/event/${id}`, {
+        method: 'DELETE'
+    })
 }
