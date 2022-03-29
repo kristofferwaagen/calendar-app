ba3f654b301d43fa685757a0e156f03090705900
Wågen <kwa045@uib.no>
2022-03-29 23:50:42 +0200

    added getters and setters

---
+++ src/usable/event.js
@@ -0,0 +1,10 @@
+import { ref } from 'vue'
+import { fetchEvents, storeEvent, muteateEvent, destroyEvent } from '../api/eventTracker'
+
+export default function useEvents() {
+    const getEvents = ref([])
+
+    const setEvents = async () => {
+
+    }
+}
